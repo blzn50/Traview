f63371fdb2665195b10126254e1f0d418e81c6cd
Kha Nguyen <khanguyen@gmail.com>
2017-05-04 14:20:12 +0300

    add review endpoints

--- src/main/java/com/review/model/ReviewRepo.java
+++ src/main/java/com/review/model/ReviewRepo.java
@@ -1,7 +1,10 @@
 package com.review.model;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by khanguyen on 4/30/17.
  */
-public interface ReviewRepo {
+public interface ReviewRepo extends JpaRepository<Review, Long> {
+
 }
