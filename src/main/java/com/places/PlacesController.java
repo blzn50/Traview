5cd2dc385fe38f585894b65fa7e55ae21732d44e
Kha Nguyen <khanguyen@gmail.com>
2017-05-09 21:44:34 +0300

    Add recommend feature

--- src/main/java/com/places/PlacesController.java
+++ src/main/java/com/places/PlacesController.java
@@ -6,15 +6,28 @@ import com.account.model.AccountRepo;
 import com.places.model.Place;
 import com.review.model.Review;
 import com.review.model.ReviewRepo;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +36,15 @@ import java.util.List;
  */
 @RestController
 public class PlacesController {
+    @Autowired
+    private DataSource dataSource;
+    private List<RecommendedItem> recommendedItems;
+    @Autowired
+    private UserBasedRecommender userBasedRecommender;
+    @Autowired
+    private ReviewRepo reviewRepo;
+    @Autowired
+    private AccountRepo accountRepo;
     @RequestMapping("/search")
     public ResponseEntity<?> keywordSearch(@RequestParam("keyword") String keyword) {
         List<Place> searchResults = new ArrayList<>();
@@ -32,24 +54,47 @@ public class PlacesController {
         JSONArray result = jsonObject.getJSONArray("results");
         for (int i = 0; i < result.length(); i++) {
             String placeId = result.getJSONObject(i).getString("place_id");
-            String detailSearchUrl = Utils.apiPlaceDetailSearchUrl + Utils.place_id + placeId;
-            jsonResponse = Utils.getJsonFromGetRequest(detailSearchUrl);
-            JSONObject curResult = new JSONObject(jsonResponse.trim()).getJSONObject("result");
-            Place placeDetail = new Place();
-            placeDetail.setPlaceId(curResult.getString("place_id"));
-            placeDetail.setAddress(curResult.getString("formatted_address"));
-            placeDetail.setPlaceName(curResult.getString("name"));
-            placeDetail.setType(curResult.getJSONArray("types").getString(0));
-            List<String> photoRefs = new ArrayList<>();
-            if (curResult.has("photos")) {
-                JSONArray photos = curResult.getJSONArray("photos");
-                for (int j = 0; j < photos.length(); j++) {
-                    photoRefs.add(Utils.apiPhotoUrl + photos.getJSONObject(j).getString("photo_reference"));
-                }
-                placeDetail.setPhotos(photoRefs);
-            }
+            Place placeDetail = getPlaceDetailFromId(placeId);
             searchResults.add(placeDetail);
         }
         return new ResponseEntity<>(searchResults, HttpStatus.OK);
+    }
+    @RequestMapping("/recommend")
+    public ResponseEntity<?> recommendPlace() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Account currentUser = accountRepo.findByUsername(username);
+        try {
+            recommendedItems = userBasedRecommender.recommend(currentUser.getUserid(), 4);
+        } catch (TasteException e) {
+            e.printStackTrace();
+            recommendedItems = new ArrayList<>();
+        }
+        List<Place> searchResults = new ArrayList<>();
+        for (RecommendedItem recommendation : recommendedItems) {
+            String placeId = reviewRepo.findPlaceIdByHashCode(recommendation.getItemID());
+            Place placeDetail = getPlaceDetailFromId(placeId);
+            searchResults.add(placeDetail);
+        }
+        return new ResponseEntity<>(searchResults, HttpStatus.OK);
+    }
+    private Place getPlaceDetailFromId(String placeId) {
+        String detailSearchUrl = Utils.apiPlaceDetailSearchUrl + Utils.place_id + placeId;
+        String jsonResponse = Utils.getJsonFromGetRequest(detailSearchUrl);
+        JSONObject curResult = new JSONObject(jsonResponse.trim()).getJSONObject("result");
+        Place placeDetail = new Place();
+        placeDetail.setPlaceId(curResult.getString("place_id"));
+        placeDetail.setAddress(curResult.getString("formatted_address"));
+        placeDetail.setPlaceName(curResult.getString("name"));
+        placeDetail.setType(curResult.getJSONArray("types").getString(0));
+        List<String> photoRefs = new ArrayList<>();
+        if (curResult.has("photos")) {
+            JSONArray photos = curResult.getJSONArray("photos");
+            for (int j = 0; j < photos.length(); j++) {
+                photoRefs.add(Utils.apiPhotoUrl + photos.getJSONObject(j).getString("photo_reference"));
+            }
+            placeDetail.setPhotos(photoRefs);
+        }
+        return placeDetail;
     }
 }
--- src/main/java/com/places/Recommender.java
+++ src/main/java/com/places/Recommender.java
@@ -1,14 +1,106 @@
-//package com.places;
-//
-//import org.apache.mahout.cf.taste.model.DataModel;
-//
-///**
-// * Created by khanguyen on 5/6/17.
-// */
-//public class Recommender {
-//    public static void main(String args[]) {
+package com.places;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.model.JDBCDataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by khanguyen on 5/6/17.
+ */
+@Component
+public class Recommender {
+    @Autowired
+    private DataSource dataSource;
+
+    public List<RecommendedItem> getRecommendedItems() {
+        return recommendedItems;
+    }
+
+    public void setRecommendedItems(List<RecommendedItem> recommendedItems) {
+        this.recommendedItems = recommendedItems;
+    }
+
+    //    DataModel dataModel = new MySQLJDBCDataModel(dataSource, "review", "user_id", "mahout_place_id", "rating", "time");
+//    UserSimilarity userSimilarity = new LogLikelihoodSimilarity(dataModel);
+//    List<RecommendedItem> recommendedItems = new ArrayList<>();
+//    UserNeighborhood userNeighborhood;
 //        try {
-//            DataModel
+//        userNeighborhood = new NearestNUserNeighborhood(10, userSimilarity, dataModel);
+//        UserBasedRecommender recommender = new GenericUserBasedRecommender(dataModel, userNeighborhood, userSimilarity);
+//        recommendedItems = recommender.recommend(2, 3);
+//
+//    } catch (TasteException e) {
+//        e.printStackTrace();
+//    }
+//        for (RecommendedItem recommendation : recommendedItems) {
+//        System.out.println(recommendation);
+//    }
+    private List<RecommendedItem> recommendedItems = new ArrayList<>();
+    private UserBasedRecommender recommender;
+    @PostConstruct
+    public void buildingRecommender() {
+        DataModel dataModel;
+        JDBCDataModel dm = new MySQLJDBCDataModel(dataSource, "review", "user_id", "mahout_place_id", "rating", "time");
+        try {
+            dataModel = new ReloadFromJDBCDataModel(dm);
+        } catch (TasteException e) {
+            e.printStackTrace();
+            return;
+        }
+        UserSimilarity userSimilarity = new LogLikelihoodSimilarity(dataModel);
+        UserNeighborhood userNeighborhood;
+        try {
+            userNeighborhood = new NearestNUserNeighborhood(10, userSimilarity, dataModel);
+            recommender = new GenericUserBasedRecommender(dataModel, userNeighborhood, userSimilarity);
+//            recommendedItems = recommender.recommend(2, 4);
+        } catch (TasteException e) {
+            e.printStackTrace();
+        }
+//        System.out.println("post construct");
+//        for (RecommendedItem recommendation : recommendedItems) {
+//            System.out.println(recommendation);
+//        }
+//        System.out.println("time 1 ");
+//        try {
+//            recommendedItems = recommender.recommend(3, 4);
+//        } catch (TasteException e) {
+//            e.printStackTrace();
+//        }
+//        for (RecommendedItem recommendation : recommendedItems) {
+//            System.out.println(recommendation);
+//        }
+//        System.out.println("time 2 ");
+    }
+    @Bean(name = "userBasedRecommender")
+    public UserBasedRecommender userBasedRecommender() {
+        return recommender;
+    }
+//    public List getResult(long userid) {
+//        try {
+//            return recommender.recommend(userid, 4);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return new ArrayList<>();
 //        }
 //    }
-//}
+}
