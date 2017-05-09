f1412d95322c5551071c58e33efbffb466af092b
Kha Nguyen <khanguyen@gmail.com>
2017-05-09 21:43:45 +0300

    add Account model and JPA repo

--- src/main/java/com/account/model/AccountRepo.java
+++ src/main/java/com/account/model/AccountRepo.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AccountRepo extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
 }
