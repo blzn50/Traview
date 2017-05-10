510813935184610b24f94ffba1cea5015060bbff
Kha Nguyen <khanguyen@gmail.com>
2017-05-10 13:51:30 +0300

    add success and failure handler for login endpoint

--- src/main/java/com/main/SecurityConfig.java
+++ src/main/java/com/main/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.main;
 
+import com.account.model.AccountRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +22,8 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     DataSource dataSource;
+    @Autowired
+    AccountRepo accountRepo;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
@@ -32,6 +37,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    System.out.println(authentication.getName());
+                    httpServletResponse.setContentType("application/json");
+                    ObjectMapper mapper = new ObjectMapper();
+                    String jsonString = mapper.writeValueAsString(accountRepo.findByUsername(authentication.getName()));
+                    httpServletResponse.setCharacterEncoding("UTF-8");
+                    httpServletResponse.getWriter().write(jsonString);
+
+                })
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                    httpServletResponse.setContentType("application/json");
+                    ObjectMapper mapper = new ObjectMapper();
+                    String jsonString = mapper.writeValueAsString("Login failed - Wrong password");
+                    httpServletResponse.setCharacterEncoding("UTF-8");
+                    httpServletResponse.setStatus(HttpStatus.FORBIDDEN.value());
+                    httpServletResponse.getWriter().write(jsonString);
+                })
                 .and()
                 .httpBasic()
                 .and()
